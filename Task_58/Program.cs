9ec14fd891d2d37c03b316a1fac87b2dfc41803a
Lupu Daniel <dlupu666@gmail.com>
2023-04-19 09:35:38 +0300

    Task_58

---
+++ Task_58/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 58: Задайте две матрицы.
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix1 = {
+            { 2, 4 },
+            { 3, 2 }
+        };
+
+        int[,] matrix2 = {
+            { 3, 4 },
+            { 3, 3 }
+        };
+
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            Console.WriteLine("Невозможно выполнить умножение матриц: количество столбцов первой матрицы должно быть равно количеству строк второй матрицы.");
+            return;
+        }
+
+        int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+
+        Console.WriteLine("Результирующая матрица:");
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                Console.Write(resultMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
